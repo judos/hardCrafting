02337b840176c910876986b2ea26da703bcb5f2a
judos <judos@gmx.ch>
2016-01-21 00:58:43 +0100

    unified control code, added signal requester

--- source/control.lua
+++ source/control.lua
@@ -1,4 +1,5 @@
 require "defines"
+require "control.signalRequester"
 
 -- Init --
 script.on_init(function()
@@ -10,26 +11,28 @@ script.on_load(function()
 end)
 
 function init()
-	if global.hardCrafting == nil then 
-		global.hardCrafting = {
-			signalReceiver = {}
-		}
-	end
+	if global.hardCrafting == nil then global.hardCrafting = {} end
+	local hc = global.hardCrafting
+	if hc.signalReceiver == nil then hc.signalReceiver = {} end
+	if hc.signalRequester == nil then hc.signalRequester = {} end
 end
 
 -- Setup and destroy --
 script.on_event(defines.events.on_built_entity, function(event)
-	createSignalReceiver(event.created_entity)
+	onBuiltEntity(event.created_entity)
 end)
 
 script.on_event(defines.events.on_robot_built_entity, function(event)
-	createSignalReceiver(event.created_entity)
+	onBuiltEntity(event.created_entity)
 end)
 
-function createSignalReceiver(entity)
+function onBuiltEntity(entity)
 	if entity.name == "signal-receiver" then
 		table.insert(global.hardCrafting.signalReceiver, entity)
-		entity.operable = false -- can't open gui
+		entity.operable = false -- no gui needed
+	elseif entity.name == "circuit-signal-requester" then
+		table.insert(global.hardCrafting.signalRequester, entity)
+		entity.operable = false -- no gui needed
 	end
 end
 
@@ -43,6 +46,12 @@ end)
 
 -- Update tick --
 script.on_event(defines.events.on_tick, function(event)
+	updateSignalReceiver(event)
+	updateSignalRequester(event)
+end)
+
+
+function updateSignalReceiver(event)
 	-- update every 5 seconds
 	if game.tick % 300 ~= 0 then return end
 	
@@ -64,8 +73,4 @@ script.on_event(defines.events.on_tick, function(event)
 			global.hardCrafting.signalReceiver[k] = nil
 		end
 	end
-end)
-
-
-
-
+end
