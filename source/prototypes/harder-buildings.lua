ebef76af0ab5a3ab945f99a97691f0c044788bb6
judos <judos@gmx.ch>
2016-03-07 01:22:13 +0100

    adding metal scrap to placeable expensive recipes

--- source/prototypes/harder-buildings.lua
+++ source/prototypes/harder-buildings.lua
@@ -1,27 +1,43 @@
 -- Requirement: --
-
+require "find-raw-ingredients"
 
 -- Item: --
-addItem("iron-strut","intermediate-product","b[iron-gear-wheel]",50)
+--addItem("iron-strut","intermediate-product","b[iron-gear-wheel]",50)
+addItem("scrap-metal","raw-resource","b[iron-gear-wheel]",50)
 
 -- Recipes: --
 --       item Name     category   subgroup     time    ingredients     			products		order
-addRecipe("iron-strut","crafting",nil,0.5,{{"iron-plate",1}},{{"iron-strut",2}},nil)
+--addRecipe("iron-strut","crafting",nil,0.5,{{"iron-plate",1}},{{"iron-strut",2}},nil)
 
 
-for recipeName,t in pairs(data.raw["recipe"]) do
-	local resultItem = t.result
+for recipeName,recipe in pairs(data.raw["recipe"]) do
+	local resultItem = recipe.result
 	local item = data.raw["item"][resultItem]
 	if item and item.place_result then
-		local gear = recipeItemAmount(t,"iron-gear-wheel")
-		local plates = recipeItemAmount(t,"iron-plate")
-		print(tostring(gear)..", "..tostring(plates).." name: "..resultItem)
-		if gear>0 and plates>0 then
-			local subgroupName = item.subgroup
-			local groupName = data.raw["item-subgroup"][subgroupName].group
-			print(recipeName..", "..tostring(subgroupName)..", "..tostring(groupName).." "..serpent.block(t))
-			if groupName == "production" then
-				ChangeRecipe(recipeName,"iron-plate","iron-strut",plates*2)
+		local iron = findRawIngredient(resultItem,"iron-plate")
+		local copper = findRawIngredient(resultItem,"copper-plate")
+		
+		--warn(resultItem..": uses "..iron.." iron + "..copper.." copper")
+		
+		if iron >= 10 and copper>= 5 then
+			info(resultItem.." -> scrap")
+			local results = recipe.results
+			if not results then
+				local resultAmount = recipe.result_count or 1
+				results = { {type="item", name=recipe.result, amount=resultAmount} }
+			end
+			table.insert(results, { type="item", name="scrap-metal",amount=1})
+			recipe.results = results
+			recipe.result = nil
+			recipe.result_count = nil
+			if not recipe.icon then
+				recipe.icon = data.raw["item"][resultItem].icon
+			end
+			if not recipe.subgroup then
+				recipe.subgroup = data.raw["item"][resultItem].subgroup
+			end
+			if not recipe.order then
+				recipe.order = data.raw["item"][resultItem].order
 			end
 		end
 	end
