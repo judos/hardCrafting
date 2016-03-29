04c25cc896bd823994ea53717507701d0c9ad3f9
ZeaS <zeas@qq.com>
2016-03-29 23:11:00 +0800

    improve compatibility with some other mods

--- source/find-raw-ingredients.lua
+++ source/find-raw-ingredients.lua
@@ -44,7 +44,7 @@ function findRawIngredient(itemName,rawResourceName)
 	if firstRecipe.results then
 		for _,itemStack in pairs(firstRecipe.results) do
 			if itemStack["name"] == itemName then
-				output = itemStack["amount"]
+				output = itemStack["amount"] -- output may be nil !
 				break
 			end
 		end
@@ -61,6 +61,9 @@ function findRawIngredient(itemName,rawResourceName)
 	end
 
 	-- calculate for all sub ingredients
+	if not output then
+		output = 1
+	end
 	for name,amount in pairs(ingredients) do
 		if not rawIngredients[name] or not rawIngredients[name][rawResourceName] then
 			findRawIngredient(name,rawResourceName)
