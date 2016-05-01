524cfd5ca7f5806ba4394f86289f84b49e18e1e4
judos <judos@gmx.ch>
2016-05-01 22:25:20 +0200

    wip crusher lets you select recipe

--- source/prototypes/pulverizer.lua
+++ source/prototypes/pulverizer.lua
@@ -30,19 +30,21 @@ data:extend({
 -- Entity
 local pulverizer = deepcopy(data.raw["assembling-machine"]["assembling-machine-2"])
 pulverizer.name = "pulverizer"
-pulverizer.icon =  "__hardCrafting__/graphics/icons/pulverizer.png"
-pulverizer.animation = deepcopy(data.raw["furnace"]["electric-furnace"].animation)
-pulverizer.animation.filename="__hardCrafting__/graphics/entity/crusher/crusher-base.png"
-pulverizer.icon = "__hardCrafting__/graphics/icons/pulverizer.png"
-pulverizer.working_sound = data.raw["furnace"]["electric-furnace"].working_sound
-pulverizer.open_sound  = data.raw["furnace"]["electric-furnace"].open_sound
-pulverizer.close_sound  = data.raw["furnace"]["electric-furnace"].close_sound 
 pulverizer.crafting_categories = {"pulverizer"}
 pulverizer.energy_usage = "140kW"
 pulverizer.source_inventory_size = 3
 pulverizer.result_inventory_size = 3
 pulverizer.crafting_speed = 1
 pulverizer.minable.result = "pulverizer"
+
+pulverizer.icon =  "__hardCrafting__/graphics/icons/pulverizer.png"
+pulverizer.animation = deepcopy(data.raw["furnace"]["electric-furnace"].animation)
+pulverizer.animation.filename="__hardCrafting__/graphics/entity/crusher/crusher-base.png"
+
+pulverizer.working_sound = data.raw["furnace"]["electric-furnace"].working_sound
+pulverizer.open_sound  = data.raw["furnace"]["electric-furnace"].open_sound
+pulverizer.close_sound  = data.raw["furnace"]["electric-furnace"].close_sound 
+
 data:extend({ pulverizer })
 
 -- technology
