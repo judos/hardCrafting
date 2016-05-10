d8a7fc1f0b6183fc398bb036a693ab74e4e0e256
judos <judos@gmx.ch>
2016-05-10 18:17:03 +0200

    walls less expensive

--- source/prototypes/more-expensive-walls.lua
+++ source/prototypes/more-expensive-walls.lua
@@ -1,4 +1,4 @@
 require "libs.functions"
 
-ChangeRecipe("stone-wall", "stone-brick", "stone-brick", 20)
+ChangeRecipe("stone-wall", "stone-brick", "stone-brick", 10)
 data.raw["recipe"]["stone-wall"].energy_required = 2.5
