d8d8bba068c9469d2add58cbcd4b3d0f0e67a996
judos <judos@gmx.ch>
2016-05-14 14:00:37 +0200

    updated comment about 0.3.12 migration

--- source/control/migration_0_3_12.lua
+++ source/control/migration_0_3_12.lua
@@ -2,5 +2,5 @@
 function migration_0_3_12()
 	global.entities_cleanup_required = true
 	global.hardCrafting.version = "0.3.12"
-	info("Migration done to 0.3.12")
+	info("Migration done to 0.3.12 - will do a schedule table cleanup")
 end
