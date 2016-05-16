10f037dc0a1da94f14c67f56b45e79f4611cdb26
judos <judos@gmx.ch>
2016-05-17 00:03:59 +0200

    formatting

--- source/libs/gui.lua
+++ source/libs/gui.lua
@@ -114,13 +114,12 @@ end
 --------------------------------------------------
 
 script.on_event(defines.events.on_gui_click, function(event)
-
-		if event.element.style and event.element.style.name then
-			if event.element.style.name:starts("item-") then
-				event.element.state = true
-			end
+	if event.element.style and event.element.style.name then
+		if event.element.style.name:starts("item-") then
+			event.element.state = true
 		end
-		local player = game.players[event.player_index]
-		local uiComponentIdentifier = event.element.name
-		handleEvent(uiComponentIdentifier,player)
+	end
+	local player = game.players[event.player_index]
+	local uiComponentIdentifier = event.element.name
+	return handleEvent(uiComponentIdentifier,player)
 end)
