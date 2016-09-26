fe7d77d6fb5a85afe2370ca80b7baef88202c7c9
judos <judos@gmx.ch>
2016-09-26 21:36:58 +0200

    fix compatibility issue with Angel's Petro Chemical Processing

--- source/prototypes/easier-but-more-trains.lua
+++ source/prototypes/easier-but-more-trains.lua
@@ -1,17 +1,29 @@
 -- Half the cost of diesel-locomotive
 for _,materialCost in pairs(data.raw["recipe"]["diesel-locomotive"].ingredients) do
-	materialCost[2] = math.ceil(materialCost[2]/2)
+	if materialCost["amount"] then
+		materialCost["amount"] = math.ceil(materialCost["amount"] / 2)
+	elseif materialCost[2] then
+		materialCost[2] = math.ceil(materialCost[2] / 2)
+	end
 end
 
 -- Half the cost of cargo wagons
 for _,materialCost in pairs(data.raw["recipe"]["cargo-wagon"].ingredients) do
-	materialCost[2] = math.ceil(materialCost[2]/2)
+	if materialCost["amount"] then
+		materialCost["amount"] = math.ceil(materialCost["amount"] / 2)
+	elseif materialCost[2] then
+		materialCost[2] = math.ceil(materialCost[2] / 2)
+	end
 end
 
 -- enforce Smaller inventory for trains
 for name,table in pairs(data.raw["cargo-wagon"]) do
-	table.inventory_size = math.ceil(table.inventory_size / 2)
-	info(name.." inventory_size = "..table.inventory_size)
-	table.weight = math.ceil(table.weight / 2)
+	if table.inventory_size then
+		table.inventory_size = math.ceil(table.inventory_size / 2)
+		info(name.." inventory_size = "..table.inventory_size)
+	end
+	if table.weight then
+		table.weight = math.ceil(table.weight / 2)
+	end
 end
 
