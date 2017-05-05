7431164d55bbbce7cefe6ed862bf731c260a5795
judos <judos@gmx.ch>
2017-05-05 21:34:29 +0200

    fix names of recipes which should produce waste metal

--- source/prototypes/harder-buildings.lua
+++ source/prototypes/harder-buildings.lua
@@ -1,12 +1,16 @@
 -- Requirement: --
 
 local recipeWhiteList = table.set({
-	"gun-turret", "laser-turret", "radar", "rocket-silo", "basic-splitter", "fast-splitter", "express-splitter", "filter-inserter",  
-	"big-electric-pole", "substation", "train-stop", "diesel-locomotive", "tank", "logistic-robot", 
-	"construction-robot", "logistic-chest-active-provider", "logistic-chest-passive-provider", 
-	"logistic-chest-requester", "logistic-chest-storage", "roboport", 
-	"solar-panel", "basic-accumulator", "pumpjack", "electric-furnace", "electric-incinerator", "pulverizer", "assembling-machine-2", 
-	"assembling-machine-3", "oil-refinery", "chemical-plant", "lab", "basic-beacon" })
+	"splitter", "fast-splitter", "express-splitter", "filter-inserter","stack-inserter","stack-filter-inserter",
+	"logistic-robot", "construction-robot", "roboport",
+		"logistic-chest-active-provider", "logistic-chest-passive-provider", "logistic-chest-requester", "logistic-chest-storage", 
+	"big-electric-pole", "substation", "train-stop", "locomotive", "tank",  
+	"solar-panel", "accumulator",
+	"pumpjack", "electric-furnace", "electric-incinerator", "pulverizer", "assembling-machine-2", 
+	"assembling-machine-3", "oil-refinery", "chemical-plant", "lab", "beacon", "centrifuge",
+	"nuclear-reactor", "steam-turbine", "heat-exchanger", "heat-pipe",
+	"gun-turret", "laser-turret", "radar", "rocket-silo"   
+})
 
 -- Item: --
 addItem("scrap-metal","raw-resource","z[scrap-metal]",50)
@@ -19,30 +23,23 @@ for recipeName,recipe in pairs(data.raw["recipe"]) do
 	local resultItem = recipe.result
 	local item = data.raw["item"][resultItem]
 	if item and item.place_result and recipeWhiteList[recipeName] then
-		local iron = findRawIngredient(resultItem,"iron-plate")
-		local copper = findRawIngredient(resultItem,"copper-plate")
-		--warn(resultItem..": uses "..iron.." iron + "..copper.." copper")
-
-		if iron >= 10 and copper>= 5 then
-			--info(resultItem.." -> scrap")
-			local results = recipe.results
-			if not results then
-				local resultAmount = recipe.result_count or 1
-				results = { {type="item", name=recipe.result, amount=resultAmount} }
-			end
-			table.insert(results, { type="item", name="scrap-metal",amount=1})
-			recipe.results = results
-			recipe.result = nil
-			recipe.result_count = nil
-			if not recipe.icon then
-				recipe.icon = data.raw["item"][resultItem].icon
-			end
-			if not recipe.subgroup then
-				recipe.subgroup = data.raw["item"][resultItem].subgroup
-			end
-			if not recipe.order then
-				recipe.order = data.raw["item"][resultItem].order
-			end
+		local results = recipe.results
+		if not results then
+			local resultAmount = recipe.result_count or 1
+			results = { {type="item", name=recipe.result, amount=resultAmount} }
+		end
+		table.insert(results, { type="item", name="scrap-metal",amount=1})
+		recipe.results = results
+		recipe.result = nil
+		recipe.result_count = nil
+		if not recipe.icon then
+			recipe.icon = data.raw["item"][resultItem].icon
+		end
+		if not recipe.subgroup then
+			recipe.subgroup = data.raw["item"][resultItem].subgroup
+		end
+		if not recipe.order then
+			recipe.order = data.raw["item"][resultItem].order
 		end
 	end
 end
