f4fec43d3f27273f48457238a3079310062f6c52
judos <judos@gmx.ch>
2017-05-25 15:52:54 +0200

    landfill with dirt requires landfill tech

--- source/prototypes/landfill.lua
+++ source/prototypes/landfill.lua
@@ -4,7 +4,7 @@ if data.raw["item"]["dried-dirt"] ~= nil then
 			{
 				type = "recipe",
 				name = "landfill2by2_withDirt",
-				enabled = true,
+				enabled = false,
 				energy_required = 2,
 				ingredients = {
 					{"dried-dirt", 20}
@@ -14,3 +14,5 @@ if data.raw["item"]["dried-dirt"] ~= nil then
 		})
 	end
 end
+
+addTechnologyUnlocksRecipe("landfill","landfill2by2_withDirt")
