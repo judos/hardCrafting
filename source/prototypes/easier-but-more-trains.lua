699e8eec76e3840a26b21352dc7d466a2c03e893
judos <judos@gmx.ch>
2017-08-13 16:56:13 +0200

    fix train modification error on missing recipe

--- source/prototypes/easier-but-more-trains.lua
+++ source/prototypes/easier-but-more-trains.lua
@@ -1,30 +1,39 @@
+-- Half the cost of diesel-locomotive
 if settings.startup["hardcrafting-train-modifications"].value == true then
-  -- Half the cost of diesel-locomotive
-  for _,materialCost in pairs(data.raw["recipe"]["locomotive"].ingredients) do
-	if materialCost["amount"] then
-		materialCost["amount"] = math.ceil(materialCost["amount"] / 2)
-	elseif materialCost[2] then
-		materialCost[2] = math.ceil(materialCost[2] / 2)
+	if data.raw["recipe"]["locomotive"] and data.raw["recipe"]["locomotive"].ingredients then
+		for _,materialCost in pairs(data.raw["recipe"]["locomotive"].ingredients) do
+			if materialCost["amount"] then
+				materialCost["amount"] = math.ceil(materialCost["amount"] / 2)
+			elseif materialCost[2] then
+				materialCost[2] = math.ceil(materialCost[2] / 2)
+			end
+		end
+	else
+		err("Couldn't apply train modifications because missing recipe for locomotive")
 	end
-  end
 
-  -- Half the cost of cargo wagons
-  for _,materialCost in pairs(data.raw["recipe"]["cargo-wagon"].ingredients) do
-	if materialCost["amount"] then
-		materialCost["amount"] = math.ceil(materialCost["amount"] / 2)
-	elseif materialCost[2] then
-		materialCost[2] = math.ceil(materialCost[2] / 2)
+	-- Half the cost of cargo wagons
+	if data.raw["recipe"]["cargo-wagon"] and data.raw["recipe"]["cargo-wagon"].ingredients then
+		for _,materialCost in pairs(data.raw["recipe"]["cargo-wagon"].ingredients) do
+			if materialCost["amount"] then
+				materialCost["amount"] = math.ceil(materialCost["amount"] / 2)
+			elseif materialCost[2] then
+				materialCost[2] = math.ceil(materialCost[2] / 2)
+			end
+		end
+	else
+		err("Couldn't apply train modifications because missing recipe for cargo-wagon")
 	end
-  end
 
-  -- enforce Smaller inventory for trains
-  for name,table in pairs(data.raw["cargo-wagon"]) do
-	if table.inventory_size then
-		table.inventory_size = math.ceil(table.inventory_size / 2)
-		info(name.." inventory_size = "..table.inventory_size)
+	-- enforce Smaller inventory for trains
+	for name,table in pairs(data.raw["cargo-wagon"]) do
+		if table.inventory_size then
+			table.inventory_size = math.ceil(table.inventory_size / 2)
+			info(name.." inventory_size = "..table.inventory_size)
+		end
+		if table.weight then
+			table.weight = math.ceil(table.weight / 2)
+		end
 	end
-	if table.weight then
-		table.weight = math.ceil(table.weight / 2)
-	end
-  end
 end
+
