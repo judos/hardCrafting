41500bec8b68ec7573ba68cefee134b30637bf3b
judos <judos@gmx.ch>
2017-08-19 20:12:54 +0200

    update logging constants

--- source/constants.lua
+++ source/constants.lua
@@ -4,8 +4,4 @@ modName = "hc" -- required prefix for all ui name components which can be clicke
 fullModName = "hardCrafting" -- required for logging and prototypes
 
 
-local testing = false
-if testing then
-	debug_master = true -- Master switch for debugging, prints debug stuff into the shell where factorio was started from
-	debug_level = 1 -- 1=info 2=warning 3=error
-end
+libLog.testing = true
