5cae7a3ddb8dc5520d6961380321197f5e163428
judos <judos@gmx.ch>
2018-01-21 00:46:03 +0100

    fix wrong recipe time

--- source/prototypes/rich-iron-processing.lua
+++ source/prototypes/rich-iron-processing.lua
@@ -11,7 +11,7 @@ if settings.startup["hardcrafting-rich-ores"].value == true then
 	-- Recipes: --
 	--       item Name     category   subgroup     time    ingredients     			products		order
 	-- Tier1
-	addRecipe("iron-plate|rich","smelting","iron",9,{{"rich-iron-ore",1}},					{{"iron-plate",c(1)},{"iron-slag",1}},"b2")
+	addRecipe("iron-plate|rich","smelting","iron",3.5,{{"rich-iron-ore",1}},					{{"iron-plate",c(1)},{"iron-slag",1}},"b2")
 
 	-- Tier2
 	addRecipe("crushed-iron|rich","crusher","iron",5,{{"rich-iron-ore",4}},{{"crushed-iron",4},{"iron-nugget",1}},"d2")
